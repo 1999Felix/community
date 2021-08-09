4b0be86ccbc90d59805fd0d0d12ad19b8f2b30d8
19983422178 <1621539671@qq.com>
2021-08-09 12:37:55 +0800

    发送系统通知

--- src/main/resources/static/js/discuss.js
+++ src/main/resources/static/js/discuss.js
@@ -5,10 +5,10 @@ $(function(){
 });
 
 // 点赞
-function like(btn, entityType, entityId, entityUserId) {
+function like(btn, entityType, entityId, entityUserId, postId) {
     $.post(
         CONTEXT_PATH + "/like",
-        {"entityType":entityType,"entityId":entityId,"entityUserId":entityUserId},
+        {"entityType":entityType,"entityId":entityId,"entityUserId":entityUserId,"postId":postId},
         function(data) {
             data = $.parseJSON(data);
             if(data.code == 0) {
